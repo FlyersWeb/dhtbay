1b90791e9c52af6275a9ab1cd9c1fe2bccba6e53
FlyersWeb <contact@flyers-web.org>
2015-02-12 15:49:02 +0000

    Added portal

--- launcher.sh
+++ launcher.sh
@@ -2,6 +2,8 @@
 
 aria2c -q -j 10 --enable-dht --dht-listen-port=6882 --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --bt-metadata-only=true --bt-save-metadata=true --bt-stop-timeout=30 --bt-tracker="udp://tracker.publicbt.com:80/announce,udp://tracker.openbittorrent.com:80/announce" --dir=$(pwd)/torrent -l ./logs/aria2c.log &
 sleep 10
-node ./loadDHT.js 2>1 > ./logs/download.log &
-node ./crawlDHT.js 2>1 > ./logs/crawl.log &
-node ./loadTorrent.js 2>1 > ./logs/store.log &
+node ./portal/bin/www > ./logs/server.log &
+sleep 5
+node ./crawlDHT.js > ./logs/crawl.log &
+node ./loadDHT.js > ./logs/download.log &
+node ./loadTorrent.js > ./logs/store.log &
