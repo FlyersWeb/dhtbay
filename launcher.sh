a76685dc2b1ba99e8b4b53ad255738a6307613f7
Flyers <contact@flyers-web.org>
2015-07-02 07:09:13 +0000

    Added TLS don't check certificate for Torrage

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aria2c -q -j 10 --log-level=notice --http-accept-gzip=true --follow-torrent=false --enable-dht --dht-listen-port=6882 --enable-rpc --bt-metadata-only=true --bt-save-metadata=true --bt-stop-timeout=40 --bt-tracker="udp://tracker.publicbt.com:80/announce,udp://tracker.openbittorrent.com:80/announce,udp://open.demonii.com:1337/announce" --dir=$(pwd)/torrent -l ./logs/aria2c.log &
+aria2c -q -j 10 --log-level=notice --http-accept-gzip=true --check-certificate=false --follow-torrent=false --enable-dht --dht-listen-port=6882 --enable-rpc --bt-metadata-only=true --bt-save-metadata=true --bt-stop-timeout=40 --bt-tracker="udp://tracker.publicbt.com:80/announce,udp://tracker.openbittorrent.com:80/announce,udp://open.demonii.com:1337/announce" --dir=$(pwd)/torrent -l $HOME/dht-bay/logs/aria2c.log &
 sleep 10
-node ./crawlDHT.js 2>&1 > ./logs/crawl.log &
-node ./loadDHT.js 2>&1 > ./logs/download.log &
+node $HOME/dht-bay/crawlDHT.js 2>&1 > $HOME/dht-bay/logs/crawl.log &
+node $HOME/dht-bay/loadDHT.js 2>&1 > $HOME/dht-bay/logs/download.log &
