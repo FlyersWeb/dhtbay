fcaa4d79427cf6e3fb70bbe6d5f6cb9a80b8f26e
Flyers <contact@flyers-web.org>
2015-10-23 22:05:40 +0100

    Updated launcher

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aria2c -q -j 10 --log-level=notice --http-accept-gzip=true --check-certificate=false --follow-torrent=false --enable-dht --dht-listen-port=6882 --enable-rpc --bt-metadata-only=true --bt-save-metadata=true --bt-stop-timeout=40 --bt-tracker="udp://tracker.publicbt.com:80/announce,udp://tracker.openbittorrent.com:80/announce,udp://open.demonii.com:1337/announce" --dir=$(pwd)/torrent -l $HOME/dht-bay/logs/aria2c.log &
+aria2c -q -j 10 --log-level=notice --http-accept-gzip=true --check-certificate=false --follow-torrent=false --enable-rpc --dir=$(pwd)/torrent -l $HOME/dht-bay/logs/aria2c.log &
 sleep 10
 forever start -m 10 -o $HOME/dht-bay/logs/crawl.log -e $HOME/dht-bay/logs/crawl.log $HOME/dht-bay/crawlDHT.js
 forever start -m 10 -o $HOME/dht-bay/logs/download.log -e $HOME/dht-bay/logs/download.log $HOME/dht-bay/loadDHT.js
