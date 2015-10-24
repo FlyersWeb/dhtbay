7a447e2f6c0e8403e5a0d16e51dedc4d8a607178
Flyers <contact@flyers-web.org>
2015-10-24 15:31:37 +0200

    Added projpath variable

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-aria2c -q -j 10 --log-level=notice --http-accept-gzip=true --check-certificate=false --follow-torrent=false --enable-rpc --dir=$(pwd)/torrent -l $HOME/dht-bay/logs/aria2c.log &
+PROJPATH=$HOME/Developments/dht-bay
+LOGPATH=$PROJPATH/logs
+
+aria2c -q -j 10 --log-level=notice --http-accept-gzip=true --check-certificate=false --follow-torrent=false --enable-rpc --dir=$(pwd)/torrent -l $LOGPATH/aria2c.log &
 sleep 10
-forever start -m 10 -o $HOME/dht-bay/logs/crawl.log -e $HOME/dht-bay/logs/crawl.log $HOME/dht-bay/crawlDHT.js
-forever start -m 10 -o $HOME/dht-bay/logs/download.log -e $HOME/dht-bay/logs/download.log $HOME/dht-bay/loadDHT.js
+forever start -m 10 -o $LOGPATH/crawl.log -e $LOGPATH/crawl.log $PROJPATH/crawlDHT.js
+forever start -m 10 -o $LOGPATH/download.log -e $LOGPATH/download.log $PROJPATH/loadDHT.js
