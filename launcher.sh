d966ac05ea0a05599be9bbad0e0e43dc178ffa67
Flyers <contact@flyers-web.org>
2015-12-15 14:48:54 +0000

    Updated retry number in launch script

--- launcher.sh
+++ launcher.sh
@@ -5,5 +5,5 @@ LOGPATH=$PROJPATH/logs
 
 aria2c -q -j 10 --log-level=notice --http-accept-gzip=true --check-certificate=false --follow-torrent=false --enable-rpc --dir=$(pwd)/torrent -l $LOGPATH/aria2c.log &
 sleep 10
-forever start -m 10 -o $LOGPATH/crawl.log -e $LOGPATH/crawl.log $PROJPATH/crawlDHT.js
-forever start -m 10 -o $LOGPATH/download.log -e $LOGPATH/download.log $PROJPATH/loadDHT.js
+forever start -m 0 -o $LOGPATH/crawl.log -e $LOGPATH/crawl.log $PROJPATH/crawlDHT.js
+forever start -m 0 -o $LOGPATH/download.log -e $LOGPATH/download.log $PROJPATH/loadDHT.js
