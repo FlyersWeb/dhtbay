0bce8580036cc090381b75ceb20d3dfb14519372
FlyersWeb <contact@flyers-web.org>
2016-07-29 00:26:06 +0200

    Update crawler code to be ES6 compatible

--- crawlDHT.js
+++ crawlDHT.js
@@ -1,41 +1,35 @@
-var config = require('./config/database');
-var util = require('util');
+const config = require('./config/database');
 
-var redis = require("redis");
+const DHT = require('bittorrent-dht');
+
+const redis = require("redis");
     client = redis.createClient(config.redis.port, config.redis.host, config.redis.options);
 
-console.logCopy = console.log.bind(console);
+const bunyan = require("bunyan");
+const logger = bunyan.createLogger({name: "crawler"});
 
-console.log = function(data) {
-   if(arguments.length) {
-      var timestamp = '[' + new Date().toUTCString() + ']';
-      this.logCopy(timestamp, arguments);
-   }
-};
-
-var DHT = require('bittorrent-dht');
-
-var dht = new DHT();
+const dht = new DHT();
   
 dht.listen(6881, function(){
-  console.log('now listening');
-  console.log(dht.address());
+  logger.info('now listening');
+  logger.info(dht.address());
 });
 
 dht.on('ready',function() {
-  console.log('now ready');
+  logger.info('now ready');
 });
 
 dht.on('announce', function(peer, infoHash) {
-  console.log("announce : "+peer.host + ':' + peer.port+' : '+infoHash.toString('hex'));
+  logger.info(`announce : ${peer.host}:${peer.port} : ${infoHash.toString('hex')}`);
   dht.lookup(infoHash);
   client.rpush("DHTS", infoHash.toString('hex'));
 });
 
 dht.on('peer', function(peer, infoHash, from) {
-  console.log("peer : "+peer.host + ':' + peer.port+' : '+infoHash.toString('hex'));
+  logger.info(`announce : ${peer.host}:${peer.port} : ${infoHash.toString('hex')}`);
 });
 
 dht.on('error',function(err) {
+  logger.error(err);
   dht.destroy();
 });
